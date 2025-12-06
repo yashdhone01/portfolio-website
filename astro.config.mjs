4eb6e37afa9ea69e75b234cd669bfaa2b905c32b
yashdhone01 <yash.dhone01@gmail.com>
2025-12-06 23:06:05 +0530

    Second

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
     import sitemap from '@astrojs/sitemap'; 
 
     export default defineConfig({
-      site: 'https://yash-dhone.vercel.app', // You will update this later
+      site: 'https://yashdhone.vercel.app', // You will update this later
       integrations: [sitemap()],
     });
